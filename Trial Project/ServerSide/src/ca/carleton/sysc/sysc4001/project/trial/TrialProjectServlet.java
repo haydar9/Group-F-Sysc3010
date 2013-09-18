c95e8ce1db575c64959b717e503331fae11fbf54
Haydar <haydar.iss4@gmail.com>
2013-09-17 21:00:12 -0400

    Starting project commit.
    Signed-off-by: Haydar <haydar.iss4@gmail.com>

---
+++ Trial Project/ServerSide/src/ca/carleton/sysc/sysc4001/project/trial/TrialProjectServlet.java
@@ -0,0 +1,43 @@
+package ca.carleton.sysc.sysc4001.project.trial;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TrialProjectServlet
+ */
+@WebServlet("/fservlet")
+public class TrialProjectServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3352070522499807626L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TrialProjectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
