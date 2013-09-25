bccd823d88ddd78242768e28c92ece06d163cd75
Haydar9 <haydar.iss4@gmail.com>
2013-09-24 22:29:07 -0400

    update

---
+++ Trial Project/ClientSide/src/ca/carleton/sysc4001/project/trial/java/client/Client.java
@@ -0,0 +1,26 @@
+package ca.carleton.sysc4001.project.trial.java.client;
+
+public class Client {
+
+	/**
+	 * Client class is the main class, where it can take arguments in order
+	 * to customize the client functionality.
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		//TODO: separate parsing command line arguments in a separate class or private inner class
+		//initialize connection
+		//attempt to establish connection
+		//handle error and throwback 
+		// if successful:
+		// 1 - knockknock protocol.
+		// 2 - establish state
+		// 3 - render/update client game state
+		// the game will be command line based. perhaps provide a command line interface for easier integration with GUI 
+		// .... OR ....
+		// consider RMI and serializing objects for getting model/game state
+	}
+	
+	
+}
--- Trial Project/ClientSide/src/ca/carleton/sysc4001/project/trial/java/client/Main.java
+++
@@ -1,9 +0,0 @@
-package ca.carleton.sysc4001.project.trial.java.client;
-
-public class Main {
-
-	public static void main(String[] args)
-	{
-		
-	}
-}
