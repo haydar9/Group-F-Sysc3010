f323549601027e4e8a498f95cc7181face84d0b9
haydar <haydar.iss4@gmail.com>
2013-12-04 02:25:14 -0500

    requried dependancy project
    Signed-off-by: haydar <haydar.iss4@gmail.com>

---
+++ Term Project - Communication/src/has/comm/XmlDefinition.java
@@ -0,0 +1,30 @@
+package has.comm;
+
+public class XmlDefinition {
+
+	public final static String HOME_AUTOMATION_SYSTEM = "HomeAutomationSystem" ;
+	public final static String REQUEST = "request" ;
+	public final static String LED = "led" ;
+	public final static String  UPDATE = "update";
+	public final static String  MODEL = "model";
+
+
+	//common tags
+	public final static String STATUS = "status" ;
+	public final static String VALUE = "value" ;
+
+	//update constants
+	
+	//motion sensor
+	public final static String  MOTION_SENSOR = "motion_sensor";
+	public final static String  MOTION_STILL = "motion_still";
+	public final static String  MOTION_DETECTED = "motion_detected";
+	
+	//temperature sensor
+	public final static String  TEMPERATURE_SENSOR = "temperature_sensor";
+	
+	//Fan "motor"
+	public final static String  FAN = "fan";
+		
+	
+}
